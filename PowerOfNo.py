e9db8ff814e93674082e62a133357ab0f032b9c0
YugaPriyanka <37238452+YugaPriyanka@users.noreply.github.com>
2019-02-06 11:48:52 +0530

    Update PowerOfNo.py

--- PowerOfNo.py
+++ PowerOfNo.py
@@ -1,8 +1,8 @@
-def power(base,exp):
+def powr(base,exp):
     if(exp==1):
         return(base)
     if(exp!=1):
-        return(base*power(base,exp-1))
+        return(base*powr(base,exp-1))
 base=int(input("Enter base: "))
 exp=int(input("Enter exponential value: "))
-print("Result:",power(base,exp))
+print("Result:",powr(base,exp))
