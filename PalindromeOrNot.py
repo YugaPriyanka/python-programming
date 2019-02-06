6ce0d03dc4577688edac2a0597785b59209b64fb
YugaPriyanka <37238452+YugaPriyanka@users.noreply.github.com>
2019-02-06 10:34:49 +0530

    Update PalindromeOrNot.py

--- PalindromeOrNot.py
+++ PalindromeOrNot.py
@@ -1,10 +1,10 @@
-n=int(input())
-temp=n
+x=int(input())
+temp=x
 rev=0
-while(n>0):
+while(x>0):
     dig=n%10
     rev=rev*10+dig
-    n=n//10
+    x=x//10
 if(temp==rev):
     print("yes")
 else:
